07cc909d27c55bbd28eb8fe6c16920b615986ca7
Gabriel Lenz <gabrielclenz@gmail.com>
2020-06-07 22:32:59 -0300

    feat: filter received data

--- src/controllers/loadRocketsData.js
+++ src/controllers/loadRocketsData.js
@@ -32,7 +32,16 @@ module.exports = async function loadRocketsData(req, res) {
       query,
     });
 
-    const csv = rocketsData.launches.filter(launch => launch.rocket.rocket.active === true && new Date(launch.launch_date_unix * 1000) >= Date.now())
+    const filteredData = rocketsData.launches.filter(launch => launch.rocket.rocket.active === true && new Date(launch.launch_date_unix * 1000) >= Date.now())
+
+    const csv = filteredData.map(rocket => {
+      return {
+        name: rocket.rocket.rocket.name,
+        mission_name: rocket.mission_name,
+        is_in_future: rocket.upcoming,
+        launch_date: new Date(rocket.launch_date_unix * 1000).toISOString(),
+      }
+    })
 
     return res.status(200).send(json2csv.parse(csv))
   } catch (error) {
